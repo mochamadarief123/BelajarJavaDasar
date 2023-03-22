8d931bfbce7f81d965dd64cd7b5ab5e5f0e1e796
mochamadarief <mochamadarief777@gmail>
2023-03-23 04:30:28 +0700

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 23/03/23
+ * Time: 4:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world, im ready for learn Java Programming Language");
+    }
+}
